4e27f813db54c66ae5ef3f48a9427f5d73a7526c
pupchenko <pupchenkoartur@gmail.com>
2024-03-06 13:44:13 +0200

    Added MySQL testcontainer

--- bank-account/account-query/src/test/java/com/practice/account/query/QueryApplicationTests.java
+++ bank-account/account-query/src/test/java/com/practice/account/query/QueryApplicationTests.java
@@ -2,12 +2,31 @@ package com.practice.account.query;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+@Testcontainers
 @SpringBootTest
 class QueryApplicationTests {
+
+    @Container
+    static MySQLContainer mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0-debian"));
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> mySQLContainer.getJdbcUrl());
+        registry.add("spring.datasource.driverClassName", () -> mySQLContainer.getDriverClassName());
+        registry.add("spring.datasource.username", () -> mySQLContainer.getUsername());
+        registry.add("spring.datasource.password", () -> mySQLContainer.getPassword());
+        registry.add("spring.flyway.enabled", () -> "true");
+    }
+
 
     @Test
     void contextLoads() {
     }
-
 }
